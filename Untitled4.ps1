d3c27e89a3b09973a1a821ceb2465a28b744f34c
oxycash <sankar19902006@gmail.com>
2019-07-31 21:24:29 +0530

    Create Untitled4.ps1

---
+++ Untitled4.ps1
@@ -0,0 +1,16 @@
+﻿$result = @()
+
+$VersionList = Get-Content C:\temp\s.txt | ForEach-Object {
+    $result = Invoke-WebRequest -Uri "https://www.google.com/search?client=firefox-b-d&q=$($_)"
+    $data = $result.ParsedHtml.getElementsByTagName("html") |    select -ExpandProperty InnerText
+    $vhost = $data
+
+  
+
+    [pscustomobject]@{
+        Computer = $_
+        VirtualHost = $vhost
+       
+    }
+}
+$versionList
