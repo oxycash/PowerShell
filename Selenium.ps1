363c25624c986c7586191d47fdfe126e91ac98bb
oxycash <sankar19902006@gmail.com>
2019-08-02 20:56:29 +0530

    Create Selenium.ps1

---
+++ Selenium.ps1
@@ -0,0 +1,13 @@
+﻿$Driver = Start-SeFirefox 
+Enter-SeUrl https://old.reddit.com/ -Driver $Driver
+#$driver.FindElementByName("btn btn-default redBtn").Submit()
+$driver.FindElementByName("user").SendKeys("oxycash")
+$driver.FindElementByName("passwd").sendkeys("11061990")
+$driver.FindElementByClassName("btn").submit()
+
+
+# https://ironmansoftware.com/automated-web-testing-with-selenium-and-powershell/
+#https://github.com/adamdriscoll/selenium-powershell
+#https://tech.mavericksevmont.com/blog/powershell-selenium-automate-web-browser-interactions-part-i/
+#https://github.com/adamdriscoll/selenium-powershell
+#https://www.toolsqa.com/selenium-webdriver/find-element-selenium/
